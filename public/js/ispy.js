0fdbabda791601b94788e6241d4af9c6778a2cc3
Hannah Westberg <Hannah@Hannahs-MacBook-Air.local>
2015-07-25 16:11:01 -0500

    Riddle as a list with checkboxes. Clicked items change opacity.

--- public/js/ispy.js
+++ public/js/ispy.js
@@ -1,44 +1,38 @@
 "use strict";
-
 (function(){
 	var app = angular.module("ispyModule", []);
-
 	// declaring controller and resetting scope
-	app.controller("IspyController", ["$scope", function($scope){
-			
+	app.controller("IspyController", ["$scope", function($scope){		
 			var puzzles = [];
-	
 	// ajax request to pull puzzle data from external json file
 			$.get("/js/ispyPuzzles.json").done(function(data){
 				console.log("Ajax complete!");
 				$scope.puzzles = data;
 				console.log($scope.puzzles);
-	
 	// updating scope after ajax request is completed
 				$scope.$apply();
-
 	// getting puzzle pics & riddels from json file			
-				
 				var puzzlePics = "";
-
 				var puzzleRiddle = "";
-
 			 	$scope.getPuzzle = function(){
 					puzzlePics= "";
 					$.each($scope.puzzles,function(i, puzzle){
-						console.log($scope.puzzleTitle.title);
-						puzzleRiddle = "<p>" + puzzle.riddle + "</p>";
+						
+						console.log($scope.puzzleTitle.title);	
+						
 						if (puzzle.title == $scope.puzzleTitle.title) {
+							var puzzleRiddle = "";
+							$.each(puzzle.riddle, function(i){
+								$.each(puzzle.riddle[i], function(key, value){
+									console.log(value);
+									puzzleRiddle += "<input type='checkbox' disabled>" + value + "<br>";
+								});
+							});
+
 							$.each(puzzle.images,  function(i){
 	// adding clickable class on clickable puzzle items
-						 		if (puzzle.images[i].clickable == true){
-						 			console.log(puzzle.images[i].name + " clickable is " + puzzle.images[i].clickable);
-						 			puzzlePics += "<img src=" + puzzle.images[i].src + " class='clickable'>";
-						 			
-						 		}else {
-						 			puzzlePics += "<img src=" + puzzle.images[i].src + ">";
-						 			console.log(puzzle.images[i].name + " clickable is " + puzzle.images[i].clickable);
-						 		}
+						 			console.log(puzzle.images[i].name + " classes are " + puzzle.images[i].classes);
+						 			puzzlePics += "<img src=" + puzzle.images[i].src + " class="  + "'" + puzzle.images[i].classes + "'" + ">";	
 							});
 							$(".puzzle-riddle-container").html(puzzleRiddle);
 						}	
@@ -46,10 +40,19 @@
 					console.log("puzzlepic called");
 					console.log(puzzlePics);
 					$(".puzzle-container").html(puzzlePics);
+
+					$(".clickable").click(function(){
+					console.log("clicked");
+					$(this).addClass("clicked");
+				});
 				}
+				
+				
 
 				}).fail(function(d, textstatus, error){
 					console.log("There was an error loading your datas!" + textstatus + error);
 			});
 	}]);
 })();
+
+
