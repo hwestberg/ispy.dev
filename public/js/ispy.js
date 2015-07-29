c2af3c46529e35bf00cf44005d01a40c20201e4e
Hannah Westberg <Hannah@Hannahs-MacBook-Air.local>
2015-07-29 09:31:16 -0500

    Update with Angular insted of vanilla JS

--- public/js/ispy.js
+++ public/js/ispy.js
@@ -3,7 +3,9 @@
 	var app = angular.module("ispyModule", []);
 	// declaring controller and resetting scope
 	app.controller("IspyController", ["$scope", function($scope){		
-			var puzzles = [];
+			$scope.riddles = [];
+			$scope.images = [];
+			$scope.title = "";
 	// ajax request to pull puzzle data from external json file
 			$.get("/js/ispyPuzzles.json").done(function(data){
 				console.log("Ajax complete!");
@@ -11,48 +13,30 @@
 				console.log($scope.puzzles);
 	// updating scope after ajax request is completed
 				$scope.$apply();
-	// getting puzzle pics & riddels from json file			
-				var puzzlePics = "";
-				var puzzleRiddle = "";
+	// getting puzzle pics & riddles from json file			
 			 	$scope.getPuzzle = function(){
-					puzzlePics= "";
 					$.each($scope.puzzles,function(i, puzzle){
-						
-						console.log($scope.puzzleTitle.title);	
-						
-						if (puzzle.title == $scope.puzzleTitle.title) {
-							var puzzleRiddle = "";
-							$.each(puzzle.riddle, function(i){
-								$.each(puzzle.riddle[i], function(key, value){
-									console.log(value);
-									puzzleRiddle += "<input type='checkbox' disabled>" + value + "<br>";
-								});
-							});
-
-							$.each(puzzle.images,  function(i){
-	// adding clickable class on clickable puzzle items
-						 			console.log(puzzle.images[i].name + " classes are " + puzzle.images[i].classes);
-						 			puzzlePics += "<img src=" + puzzle.images[i].src + " class="  + "'" + puzzle.images[i].classes + "'" + ">";	
-							});
-							$(".puzzle-riddle-container").html(puzzleRiddle);
+						console.log("puzzle title is" + $scope.puzzleTitle.title);	
+	// selecting only images and riddles from user-selected puzzle
+						if (puzzle.title == $scope.puzzleTitle.title) {	
+							$scope.riddles = puzzle.riddle;
+							console.log(puzzle.riddle);
+							$scope.images = puzzle.images;
+							console.log(puzzle.images);
 						}	
 					});
-					console.log("puzzlepic called");
-					console.log(puzzlePics);
-					$(".puzzle-container").html(puzzlePics);
-
-					$(".clickable").click(function(){
-					console.log("clicked");
-					$(this).addClass("clicked");
-				});
 				}
-				
-				
+
+				$scope.isClicked = function(event){
+					if ($(event.target).hasClass("clickable")){
+						$(event.target).addClass("clicked");
+						
+					}
+				}	
 
 				}).fail(function(d, textstatus, error){
 					console.log("There was an error loading your datas!" + textstatus + error);
 			});
 	}]);
 })();
-
 
